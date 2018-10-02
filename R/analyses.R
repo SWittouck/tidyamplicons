1b86e7c5d6b12d20fe4a5136074ad9e2d5a7921c
Stijn Wittouck <stijn.wittouck@hotmail.com>
2018-10-02 11:11:01 +0200

    Make "perform_adonis" remove samples where predictors are NA

--- R/analyses.R
+++ R/analyses.R
@@ -1,8 +1,11 @@
 # Performs adonis function of the vegan package and returns output.
 # Predictors should be a character vector.
+# Samples where one of the predictors is NA are removed.
 perform_adonis <- function(ta, predictors, permutations = 999) {
 
   abundances_matrix <- ta %>%
+    modify_at("samples", drop_na, one_of(predictors)) %>%
+    process_sample_selection() %>%
     add_rel_abundance() %>%
     abundances() %>%
     as_abundances_matrix(value = "rel_abundance")
