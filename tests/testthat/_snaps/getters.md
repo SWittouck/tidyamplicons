44c13f24115b8fc4361822d5f98bd57dd45c51e0
TheOaphidian <tim.van.rillaer@hotmail.com>
2023-04-18 11:45:26 +0200

    Add tests occurrence, mean rel ab and adonis

--- tests/testthat/_snaps/getters.md
+++ tests/testthat/_snaps/getters.md
@@ -15,3 +15,22 @@
       n_samples    n_taxa   n_reads 
             217      1957   3873478 
 
+# Perform adonis shows stable output
+
+    Code
+      adn
+    Output
+      Permutation test for adonis under reduced model
+      Terms added sequentially (first to last)
+      Permutation: free
+      Number of permutations: 999
+      
+      adonis2(formula = as.formula(paste("abundances_matrix", formula_RHS, sep = " ~ ")), data = metadata, permutations = permutations)
+                Df SumOfSqs      R2      F Pr(>F)    
+      location   1    2.800 0.03823 8.4267  0.001 ***
+      method     1    0.334 0.00456 1.0056  0.409    
+      Residual 211   70.118 0.95721                  
+      Total    213   73.253 1.00000                  
+      ---
+      Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1
+
--- tests/testthat/test-getters.R
+++ tests/testthat/test-getters.R
@@ -1,4 +1,6 @@
 ta_test <- read_tidyamplicons(test_path("data/urt"))
+len_taxa <- length(ta_test$taxa$taxon_id)
+len_samples <- 214 #some empty samples in the df
 
 test_that("Correct numbers reported", {
     expect_snapshot(ta_test %>% report_numbers())
@@ -11,11 +13,61 @@ test_that("Correct numbers reported", {
 test_that("Relative abundance matrix can be created", {
     relabun_mat <- ta_test %>% get_rel_abundance_matrix()
     expect_lte(max(relabun_mat), 1)
-    expect_equal(dim(relabun_mat), c(214, 1957))
+    expect_equal(dim(relabun_mat), c(len_samples, len_taxa))
 })
 
 test_that("Beta diversity table can be generated", {
     beta_table <- ta_test %>% get_betas()
     expect_lte(max(beta_table$beta), 1)
     expect_equal(dim(beta_table), c(22791, 17))
+})
+
+test_that("Occurences are calculated",{
+    occ <- ta_test %>% occurrences("location")
+    expect_equal(dim(occ), c(2*len_taxa, 3))
+    expect_true(all(c("location","occurrence") %in% names(occ)))
+    expect_equal(sum(occ$occurrence), 7693)
+})
+
+test_that("Presence/absense is calculated", {
+    pres_ab <- ta_test %>% occurrences("location", pres_abs=TRUE)
+    expect_equal(dim(pres_ab), c(4*len_taxa, 4))
+    expect_true(all(c("location", "presence", "n") %in% names(pres_ab)))
+    expect_equal(sum(pres_ab$n), len_samples * len_taxa)
+})
+
+test_that("Mean relative abundance is computed", {
+    mra <- ta_test %>% mean_rel_abundances()
+    expect_equal(sum(mra$mean_rel_abundance), 1)
+    expect_equal(length(mra$taxon_id), len_taxa)
+})
+
+test_that("Mean relative abundance is computed for a condition", {
+    mra <- ta_test %>% mean_rel_abundances("location")
+    cond_opt <- unique(ta_test$samples$location)
+    expect_equal(sum(mra$mean_rel_abundance), length(cond_opt))
+    expect_equal(length(mra$taxon_id), len_taxa*length(cond_opt))
+})
+
+test_that("Extract sample tibble", {
+    expect_equal(ta_test %>% samples(), ta_test$samples)
+})
+
+test_that("Extract taxon tibble", {
+    expect_equal(ta_test %>% taxa(), ta_test$taxa)
+})
+
+test_that("Extract abundances tibble", {
+    expect_equal(ta_test %>% abundances(), ta_test$abundances)
+})
+
+test_that("Extract all three tables in one large tibble",{
+    wide_ta <- ta_test %>% everything()
+    expect_equal(dim(wide_ta), c(7693, 18))
+})
+
+test_that("Perform adonis shows stable output", {
+    set.seed(42)
+    adn <- ta_test %>% perform_adonis(c("location", "method"))
+    expect_snapshot(adn)
 })
